7be83667e500632822fcbf577a614fe66f3ca06a
JiXiangChua <jixiangchua2697@gmail.com>
2024-06-28 15:58:46 +0800

    first commit

---
+++ ios/FaceIDManager.h
@@ -0,0 +1,12 @@
+//
+//  FaceIDManager.h
+//  RNNativeApp
+//
+//  Created by JI XIANG on 28/6/24.
+//
+
+#ifndef FaceIDManager_h
+#define FaceIDManager_h
+
+
+#endif /* FaceIDManager_h */
---
+++ ios/FaceIDManager.m
@@ -0,0 +1,8 @@
+//
+//  FaceIDManager.m
+//  RNNativeApp
+//
+//  Created by JI XIANG on 28/6/24.
+//
+
+#import <Foundation/Foundation.h>
